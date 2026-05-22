f38a79329377892d9d8402019d0ad98361131648
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-05-22 14:26:53 +1000

    chore: add test script. add placeholder test

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -1,7 +1,12 @@
 import usersRouter from "@/routers/users";
 import express from "express";
 import request from "supertest";
+import { describe, expect, test } from "@jest/globals";
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use("/users", usersRouter);
+
+test("hello", () => {
+  expect(1 + 2).toBe(3);
+});
