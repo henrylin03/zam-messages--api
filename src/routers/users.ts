44d9c805e642500ed850f280f462cb5b86d685b9
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-05-23 15:21:14 +1000

    chore(users): remove GET request

--- src/routers/users.ts
+++ src/routers/users.ts
@@ -3,7 +3,6 @@ import { Router } from "express";
 
 const usersRouter = Router();
 
-usersRouter.get("/", (_req, res) => res.json({ msg: "hello user" }));
 usersRouter.post("/new", ...newUserPost);
 
 export default usersRouter;
