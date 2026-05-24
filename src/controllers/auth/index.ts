19f677df56f5230b692c7f5d40dc0964ad8a71e2
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-05-24 13:47:02 +1000

    feat(auth): add endpoint /validate to confirm jwt is valid. responding with status 200 and message 'jwt ok' if ok
    this will be called when trying to access a protected route from the frontend.

--- src/controllers/auth/index.ts
+++ src/controllers/auth/index.ts
@@ -1,7 +1,15 @@
+import { Request, Response } from "express";
 import passport from "../../config/passport";
 import { createJwt } from "./createJwt";
 
 export const loginPost = [
   passport.authenticate("local", { session: false }),
   createJwt,
+];
+
+export const validateJwtGet = [
+  passport.authenticate("jwt", { session: false }),
+  (_req: Request, res: Response) => {
+    return res.status(200).json({ msg: "JWT ok" });
+  },
 ];
