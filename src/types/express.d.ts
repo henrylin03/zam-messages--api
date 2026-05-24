69ec91fe3b94b5a3f2a422a89c5fb77aa7e7c348
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-05-24 14:09:24 +1000

    chore: install prettier. ignore files in coverage and dist/. apply prettier to all files in codebase.

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,7 +1,7 @@
 import type { User as PrismaUser } from "../../generated/prisma/client";
 
 declare global {
-    namespace Express {
-        interface User extends PrismaUser {}
-    }
+  namespace Express {
+    interface User extends PrismaUser {}
+  }
 }
